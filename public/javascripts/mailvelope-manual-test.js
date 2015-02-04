57dba9054b5cb471f11b53c3a3a34f98a0d3f96f
Thomas Oberndörfer <thomas@mailvelope.com>
2015-02-04 15:52:35 +0100

    Measure time in manual tests

--- public/javascripts/mailvelope-manual-test.js
+++ public/javascripts/mailvelope-manual-test.js
@@ -12,13 +12,14 @@ function init() {
   $('#clearBtn').on('click', function() {
     $('#editor_cont, #display_cont').empty();
     $('#encryptBtn').off('click');
-    $('#armored_msg').val('');
+    $('#armored_msg, #encryptTime, #decryptTime').val('');
     initEditor();
   });
 
   $('#initEditorBtn').on('click', function() {
     $('#editor_cont').empty();
     $('#encryptBtn').off('click');
+    $('#encryptTime').val('');
     initEditor();
   });
 
@@ -32,7 +33,9 @@ function init() {
   function initEditor() {
     mailvelope.createEditorContainer('#editor_cont', keyring).then(function(editor) {
       $('#encryptBtn').on('click', function() {
+        var t0 = performance.now();
         editor.encrypt(['test@mailvelope.com']).then(function(armored) {
+          $('#encryptTime').val(parseInt(performance.now() - t0));
           $('#armored_msg').val(armored);
         });
       });
@@ -41,6 +44,9 @@ function init() {
 
   $('#decryptBtn').on('click', function() {
     $('#display_cont').empty();
-    mailvelope.createDisplayContainer('#display_cont', $('#armored_msg').val(), keyring);
+    var t0 = performance.now();
+    mailvelope.createDisplayContainer('#display_cont', $('#armored_msg').val(), keyring).then(function() {
+      $('#decryptTime').val(parseInt(performance.now() - t0));
+    });
   });
 }

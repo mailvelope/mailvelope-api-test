1128a7b51b5894ae1314899dcce0c5fc5631017d
Remy Bertot <remy@passbolt.com>
2018-05-31 16:14:48 +0530

    Display a warning if javacript is not enabled or if mailvelope is not installed

--- public/javascripts/form-test.js
+++ public/javascripts/form-test.js
@@ -1,49 +1,41 @@
 $(document).ready(function() {
 
+  $('html').removeClass('no-js');
+
   $('pgp-encrypted-form').each(function() {
     let pgpFormDefined = false;
     let pgpForm = this;
 
-    function handleError(message) {
-      $('<div/>', {class: 'error', text: message}).appendTo(this);
-      $(this).addClass('error');
+    function onError(message) {
+      $('<div/>', {class: 'bg-danger message', text: message}).appendTo(pgpForm);
+      $(pgpForm).addClass('error');
     }
 
     // Custom element events
-    pgpForm.addEventListener('error', function (error) {
-      console.log(error);
-      $(this).remove();
-      const $error = $('<p/>', {
-        class: 'bg-danger',
-        style: 'padding:1em'
-      }).text(error.message);
-      $('.container').append($error);
-    });
+    pgpForm.addEventListener('error', error => onError(error.message));
 
-    pgpForm.addEventListener('connected', function () {
+    pgpForm.addEventListener('connected', () => {
       pgpFormDefined = true;
-      $(this).addClass('defined');
+      $(pgpForm).addClass('defined');
     });
 
-    pgpForm.addEventListener('encrypt', function (event) {
-      $(this).remove();
+    pgpForm.addEventListener('encrypt', event => {
+      $(pgpForm).remove();
       const response = $('<pre/>').text(event.detail.armoredData);
-      const success = $('<p/>', {
-        class: 'bg-success',
-        style: 'padding:1em'
-      }).text('Success: mailvelope api returned the following content');
+      const success = $('<p/>', {class: 'message bg-success'})
+        .text('Success: mailvelope api returned the following content');
       $('.container').append(success).append(response);
     });
 
     // Support check
     // if pgpForm is not connected after 10sec we consider it absent
-    setTimeout(() => {
+    setTimeout(function() {
       if (!pgpFormDefined) {
         const errorMsg = 'OpenPGP Encrypted Form is not supported. Please install mailvelope and add this domain as an email provider.';
-        handleError(errorMsg);
+        onError(errorMsg);
       } else {
-        $(this).addClass('defined');
+        $(pgpForm).addClass('defined');
       }
-    }, 3000);
+    }, 1000);
   });
 });
